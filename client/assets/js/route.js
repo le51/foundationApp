7a3c98526a61a677b22a8dbd45914d51642e2b74
le51 <l.pfister@free.fr>
2016-01-10 16:45:55 +0100

    some ehancements in layouts

--- client/assets/js/route.js
+++ client/assets/js/route.js
@@ -1,3 +1,9 @@
 var foundationRoutes = [
-
+{"name":"music","url":"/music/","path":"templates/music.html"},
+{"name":"home.index","url":"/","path":"templates/home.index.html"},
+{"name":"computer.index","url":"/computer/","path":"templates/computer.index.html"},
+{"name":"contact","url":"/contact/","path":"templates/contact.html"},
+{"name":"home","url":"/:id","path":"templates/post.view.html"},
+//{"name":"post","url":"/post/","abstract":"true"},
+//{"name":"post.view","url":"/:id","path":"templates/post.view.html"}
 ]; 
